ab45b135e8a79c08378687bc98048ea085f7987f
b9Joker108 <beauford.a.stenberg@gmail.com>
2025-01-20 11:08:23 +0000

    Update Neovim configuration and plugins: Add obsidian and markdown extensions

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-----------------------------------------------
+-- BEAUFORD'S CONFIGURATIONS BELOW THIS LINE --
+-----------------------------------------------
+
